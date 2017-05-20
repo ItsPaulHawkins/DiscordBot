dd42cf9a4e3aeb7e0c399c56a95f3104f2686d5c
ItsPaulHawkins <hawkinsanthonypaul@gmail.com>
2017-05-20 16:37:20 -0400

    added api/plugins

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,62 @@ using System.Threading.Tasks;
 
 namespace DiscordBot
 {
-    class Program
-    {
-        static void Main(string[] args)
+        public class Program
         {
+            public static void Main(string[] args)
+                => new Program().MainAsync().GetAwaiter().GetResult();
+
+            public async Task MainAsync()
+            {
+            var client = new DiscordSocketClient();
+
+            client.Log += Log;
+
+            string token = "Mjg4MDI2NDQzMTUyMzU5NDI0.DAIinw.xtpiXGa9GgrrFd2fEXBwAgEU-Jc"; // keep this shit private 
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            client.MessageReceived += MessageRecieved;
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
-    }
+
+        private async Task MessageRecieved(SocketMessage message)
+        {
+            if (message.Content == ".ping")
+            {
+                await message.Channel.SendMessageAsync("PONNNGGGG");
+            }
+            if(message.Content == ".keith")
+            {
+                Random rand = new Random();
+                //int randomNum = rand.Next(1, 50);
+                int randomNum = 31;
+                if (randomNum >= 1 && randomNum <= 10)
+                {
+                    await message.Channel.SendMessageAsync("cars.com");
+                }
+                if(randomNum >= 11 && randomNum <= 20)
+                {
+                    await message.Channel.SendMessageAsync("I exclusively fuck miata owners.");
+                }
+                if(randomNum >= 21 && randomNum <= 30)
+                {
+                    await message.Channel.SendMessageAsync("memes, dreams, puns and guns");
+                }
+                if(randomNum >= 31 && randomNum <= 40)
+                {
+                    await message.Channel.SendMessageAsync("\"automatic transmissions are better for gas mileage\"");
+                    await message.Channel.SendFileAsync();
+                }
+            }
+        }
+        private Task Log(LogMessage msg)
+            {
+                Console.WriteLine(msg.ToString());
+                return Task.CompletedTask;
+            }
+        }
+
+
 }
