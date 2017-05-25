5dc36b300006dcf09124573e30ad107f972eef6a
ItsPaulHawkins <hawkinsanthonypaul@gmail.com>
2017-05-24 20:01:46 -0400

    trying to add voice commands

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using Discord.Audio;
+using Discord.Commands;
 
 namespace DiscordBot
 {
@@ -92,7 +94,19 @@ namespace DiscordBot
                     await message.Channel.SendMessageAsync("http://imgur.com/a/rI6jT");
                 }
             }
+   
         }
+        [Command("join")]
+        public async Task JoinChannel(IVoiceChannel channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (msg.Author as IGuildUser)?.VoiceChannel;
+            if (channel == null) { await msg.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            var audioClient = await channel.ConnectAsync();
+        }
+
         private Task Log(LogMessage msg)
             {
                 Console.WriteLine(msg.ToString());
